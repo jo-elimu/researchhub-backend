1d68143c06188d093c343ec00f851361799fe695
Kobe Attias <kobeattias@gmail.com>
2023-05-16 20:28:23 -0400

    Grobid: Adding image extraction

--- src/pipeline-grobid.py
+++ src/pipeline-grobid.py
@@ -1,65 +1,11 @@
-import base64
-import json
 import os
+import shutil
+import subprocess
 
-import fitz  # this is pymupdf
-import requests
-import xmltodict
 from lxml import etree
-from lxml.builder import E
-from PIL import Image
 
-# def extract_images_from_pdf(pdf_filepath):
-#     doc = fitz.open(pdf_filepath)
-#     images = []
-
-#     for i in range(len(doc)):
-#         for img in doc.get_page_images(i):
-#             xref = img[0]
-#             base = img[1]
-#             img_data = doc.extract_image(xref)
-#             images.append((i, base, img_data['image']))
-
-#     return images
-
-
-def extract_images_from_coordinates(pdf_filepath, coordinates, output_dir):
-    doc = fitz.open(pdf_filepath)
-    images = []
-
-    for coordinate in coordinates:
-        id, coords_string = coordinate
-        coords_list = coords_string.split(";")
-        for i, coords in enumerate(coords_list):
-            page_number, *rect = map(float, coords.split(","))
-            page = doc[int(page_number)]
-            x, y, w, h = rect
-            pix = page.get_pixmap(clip=fitz.Rect(x, y, x + w, y + h))
-            print(f"Pix width: {pix.width}, Pix height: {pix.height}, Rect: {rect}")
-            if pix.colorspace.name == "DeviceRGB":
-                img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
-            elif pix.colorspace.name == "DeviceGray":
-                img = Image.frombytes("L", [pix.width, pix.height], pix.samples)
-            else:
-                continue  # skip unsupported colorspaces
-            images.append((id, img))
-            img.save(os.path.join(output_dir, f"{id}_{i}.png"))  # Save image
-
-    return images
-
-
-def get_image_data_url(image_tuple):
-    # image_tuple format: (page_number, image_name, image_data)
-    image_data = image_tuple[2]
-    image_format = "png"  # assume PNG format, you might need to adjust this
-
-    # base64-encode the image data
-    encoded_data = base64.b64encode(image_data).decode()
-
-    # construct the data URL
-    data_url = f"data:image/{image_format};base64,{encoded_data}"
-
-    return data_url
+TEI_NAMESPACE = "http://www.tei-c.org/ns/1.0"
+NSMAP = {"tei": TEI_NAMESPACE}
 
 
 def convert_xml_to_tei(xml_filepath, xslt_filepath):
@@ -76,34 +22,10 @@ def convert_xml_to_tei(xml_filepath, xslt_filepath):
     return str(result_tree)
 
 
-# def extract_data_from_pdf(file_path):
-#     url = 'http://localhost:8070/api/processFulltextDocument'
-#     files = {'input': open(file_path, 'rb')}
-#     data = {
-#         'teiCoordinates': ['persName', 'figure', 'ref', 'biblStruct', 'formula'],
-#         'consolidateHeader': 1,
-#         'consolidateCitations': 1,
-#         'processImages': 1  # Add this line to enable image extraction
-
-#     }
-#     response = requests.post(url, files=files, data=data)
-#     return response.text
-
-import subprocess
-
-
-def extract_data_from_pdf(file_path, output_dir):
+def extract_data_from_pdf(input_dir, output_dir):
     grobid_root = "./grobid-0.7.3"
     grobid_home = f"{grobid_root}/grobid-home"  # Replace with the actual path to your Grobid installation
     grobid_jar = f"{grobid_root}/grobid-core/build/libs/grobid-core-0.7.3-onejar.jar"  # Replace with the correct Grobid JAR file
-    input_dir = "./grobid-input"
-    output_dir = "./grobid-output"
-
-    # Verify if the JAR file exists
-    if os.path.exists(grobid_jar):
-        print("Grobid JAR file exists")
-    else:
-        print("Grobid JAR file does not exist", grobid_jar)
 
     # Construct the Grobid batch command
     command = [
@@ -124,15 +46,13 @@ def extract_data_from_pdf(file_path, output_dir):
         "processFullText",
     ]
 
-    print(command)
-
     # Execute the Grobid batch command
     subprocess.run(command)
 
-    return "Extraction completed."
+    return True
 
 
-def extract_coordinates_from_tei_file(tei_filepath):
+def get_id_to_url_mapping(tei_filepath):
     tei = etree.parse(tei_filepath)
     # Get the root of the tree
     root = tei.getroot()
@@ -143,27 +63,34 @@ def extract_coordinates_from_tei_file(tei_filepath):
     }
 
     figures = root.findall(".//tei:figure", ns)
-    figure_coordinates = [
-        (figure.get("{http://www.w3.org/XML/1998/namespace}id"), figure.get("coords"))
-        for figure in figures
-    ]
-    return figure_coordinates
+
+    figure_urls = {}
+    for figure in figures:
+        figure_id = figure.get("{http://www.w3.org/XML/1998/namespace}id")
+        graphic = figure.find(".//tei:graphic", ns)
+        if graphic is not None:
+            figure_urls[figure_id] = graphic.get("url")
+
+    return figure_urls
 
 
-def write_to_file(data, filepath):
-    with open(filepath, "w") as xml_file:
-        xml_file.write(xml_data)
-
-
-TEI_NAMESPACE = "http://www.tei-c.org/ns/1.0"
-NSMAP = {"tei": TEI_NAMESPACE}
-
-
-def copy_element(src, dst_parent):
-    new_element = etree.Element(src.tag.split("}")[-1])
-    new_element.text = src.text
+def copy_element(src, dst_parent, id_to_url_mapping, assets_path):
+    if src.tag == f"{{{TEI_NAMESPACE}}}ref" and src.get("type") == "figure":
+        ref_id = src.get("target").lstrip("#")
+        img_url = id_to_url_mapping.get(ref_id)
+        if img_url is not None:
+            new_element = etree.Element("img")
+            new_element.set("src", f"{assets_path}/{img_url}")
+            new_element.set(
+                "style", "max-height: 300px; display: block; margin: 0 auto;"
+            )
+        else:
+            new_element = etree.Element(src.tag.split("}")[-1])
+    else:
+        new_element = etree.Element(src.tag.split("}")[-1])
+        new_element.text = src.text
     for child in src:
-        copy_element(child, new_element)
+        copy_element(child, new_element, id_to_url_mapping, assets_path)
     if src.tail:
         if len(dst_parent):
             last_child = dst_parent[-1]
@@ -179,7 +106,7 @@ def copy_element(src, dst_parent):
     dst_parent.append(new_element)
 
 
-def transform_tree(src, dst_parent, head_level=2):
+def transform_tree(src, dst_parent, id_to_url_mapping, assets_path, head_level=2):
     for child in src:
         if child.tag == f"{{{TEI_NAMESPACE}}}div":
             div = etree.Element("div")
@@ -206,7 +133,9 @@ def transform_tree(src, dst_parent, head_level=2):
                 div.append(head)
                 head_level = 2
             else:
-                transform_tree(child, div, min(head_level + 1, 6))
+                transform_tree(
+                    child, div, id_to_url_mapping, assets_path, min(head_level + 1, 6)
+                )
             dst_parent.append(div)
         elif child.tag == f"{{{TEI_NAMESPACE}}}head":
             head = etree.Element(f"h{head_level}")
@@ -225,10 +154,10 @@ def transform_tree(src, dst_parent, head_level=2):
                         dst_parent.text += child.tail
             dst_parent.append(head)
         else:
-            copy_element(child, dst_parent)
+            copy_element(child, dst_parent, id_to_url_mapping, assets_path)
 
 
-def convert_tei_to_html(tei_file, html_file):
+def convert_tei_to_html(tei_file, html_file, id_to_url_mapping, assets_path):
     tei_tree = etree.parse(tei_file)
     html_root = etree.Element("html")
     head = etree.SubElement(html_root, "head")
@@ -236,37 +165,54 @@ def convert_tei_to_html(tei_file, html_file):
 
     abstract = tei_tree.find(".//tei:abstract", namespaces=NSMAP)
     if abstract is not None:
-        transform_tree(abstract, body)
+        transform_tree(abstract, body, id_to_url_mapping, assets_path)
 
     tei_body = tei_tree.find(".//tei:body", namespaces=NSMAP)
     if tei_body is not None:
-        transform_tree(tei_body, body)
+        transform_tree(tei_body, body, id_to_url_mapping, assets_path)
 
     with open(html_file, "wb") as f:
         f.write(etree.tostring(html_root, method="html", pretty_print=True))
 
 
-# usage
-file_path = "./test.pdf"
-tei_filepath = "./output.tei.html"
-output_filepath = "./output.html"
-json_filepath = "./output.json"
-xslt_filepath = "./tei/json/jsonlib.xsl"
-image_dir = "./extracted-images/"
+def get_filename_without_extension(filepath):
+    return os.path.splitext(os.path.basename(filepath))[0]
 
-# images = extract_images_from_pdf(file_path)
 
-xml_data = extract_data_from_pdf(file_path, "./output")
-write_to_file(xml_data, tei_filepath)
+def copy_file(file_path, output_dir):
+    # Extract the filename without the path
+    file_name = os.path.basename(file_path)
 
-# tei_data = convert_xml_to_tei(tei_filepath, xslt_filepath)
-# coords = extract_coordinates_from_tei_file(tei_filepath)
-# print(coords)
-# images = extract_images_from_coordinates(file_path, coords, image_dir)
-# print(images)
+    # Construct the output file path in the destination directory
+    output_path = os.path.join(output_dir, file_name)
 
-# html = convert_tei_to_html(tei_filepath, output_filepath)
-# print(html)
+    # Copy the file to the destination directory
+    shutil.copy(file_path, output_path)
 
-# json_data = convert_tei_to_json(tei_filepath, json_filepath)
-# print(html_data)
+    return output_path
+
+
+# Config
+filepath = "./example2.pdf"
+input_dir = "./grobid-input"
+output_dir = "./grobid-output"
+
+# Step 1: Copy file to input directory
+filename = get_filename_without_extension(filepath)
+copy_file(filepath, input_dir)
+
+# Step 2: Extract data and images from PDF, and convert to TEI.
+extract_data_from_pdf(input_dir, output_dir)
+
+# Step 3: Get figure mapping
+tei_filepath = f"{output_dir}/{filename}.tei.xml"
+id_to_figure_mapping = get_id_to_url_mapping(tei_filepath)
+print(id_to_figure_mapping)
+
+# Step 3: Convert TEI to HTML
+
+output_html_filepath = f"{output_dir}/{filename}.html"
+assets_path = f"{filename}_assets"
+convert_tei_to_html(
+    tei_filepath, output_html_filepath, id_to_figure_mapping, assets_path
+)
